5e780ad7d70cd889eab53f4b4b3c9746bab8a082
csw3071 <csw30710@naver.com>
2025-10-16 18:17:32 +0900

    test3

---
+++ src/main/java/com/secondproject/secondproject/Entity/User.java
@@ -0,0 +1,27 @@
+package com.secondproject.secondproject.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter @Setter @ToString
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private long id;
+
+    private String name;
+
+    private String password;
+
+
+
+
+}
