509465b213694535733afc4893422590cfd83d69
hellene125 <hellene125@gmail.com>
2025-10-17 21:57:28 +0900

    251017

--- src/main/java/com/secondproject/secondproject/Entity/Assignment.java
+++ src/main/java/com/secondproject/secondproject/Entity/Assignment.java
@@ -1,4 +1,61 @@
 package com.secondproject.secondproject.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import jakarta.validation.groups.Default;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "assignment")
 public class Assignment {
+// 교수 과제 공지 테이블
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "assignment_id", columnDefinition = "BIGINT")
+    private long assignment_id; // 과제ID(PK)
+
+    @Column(name = "ass_title",nullable = false, length = 200)
+    private String title; // 공지 제목
+
+    @Column(name = "ass_content", columnDefinition = "MEDIUMTEXT",  nullable = false)
+    private String content; // 공지 본문
+
+    @Column(name = "is_enabled", columnDefinition = "TINYINT(1) NOT NULL DEFAULT 0")
+    private short is_enabled; // 제출창구(제출 마감시 비활성화(0), 제출 진행중일시 활성화(1))
+
+    @Column(name = "open_at", columnDefinition = "DATETIME NOT NULL")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate open_at; // 제출 시작일
+
+    @Column(name = "due_at", columnDefinition = "DATETIME NOT NULL")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate due_at; // 제출 마감일
+
+    @Column(name = "max_files", columnDefinition = "NOT NULL DEFAULT 3")
+    private int max_files; // 업로드 가능한 파일 최대 갯수
+
+    @Column(name = "max_size_mb", columnDefinition = "NOT NULL DEFAULT 50")
+    private int max_size_mb; // 파일 최대 용량(mb), 기본값 50
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user; // 공지한사람(=교수이름)
+
+    @Column(name = "create_at", columnDefinition = "DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime create_at; // 공지 등록일
+
+    @Column(name = "update_at", columnDefinition = "DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime update_at; // 공지 수정일
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "lecture_id", nullable = false)
+    private Lecture lecture;
 }
