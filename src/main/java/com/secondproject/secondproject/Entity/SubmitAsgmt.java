1af8aebe08c93e58c0531a86eeffcfafee5cbf1c
hellene125 <hellene125@gmail.com>
2025-10-17 21:57:28 +0900

    251017

--- src/main/java/com/secondproject/secondproject/Entity/SubmitAsgmt.java
+++ src/main/java/com/secondproject/secondproject/Entity/SubmitAsgmt.java
@@ -1,4 +1,26 @@
 package com.secondproject.secondproject.Entity;
 
+import java.time.LocalDate;
+
 public class SubmitAsgmt {
+
+    private Long submit_id;
+
+
+    private Assignment assignment;
+
+    private User user;
+
+
+    private String submit_title;
+
+    private String submit_content;
+
+    private String submit_status;
+
+    private LocalDate submit_at;
+
+    private LocalDate update_at;
+
 }
+
