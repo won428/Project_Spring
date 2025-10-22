98b6501097de424187fe78e18d4bcd39b6a79438
csw3071 <csw30710@naver.com>
2025-10-22 15:24:00 +0900

    11

--- src/main/java/com/secondproject/secondproject/Repository/CollegeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.secondproject.secondproject.Repository;
-
-import com.secondproject.secondproject.Entity.College;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CollegeRepository extends JpaRepository<College, Long> {
-    College findCollegeById(Long id);
-}
