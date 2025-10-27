df772d2dbeec7b3d02f871c5ac5dce360471c9b6
park soyoung <hellene125@gmail.com>
2025-10-27 09:44:04 +0900

    251027

--- src/main/java/com/secondproject/secondproject/controller/MajorController.java
+++ src/main/java/com/secondproject/secondproject/controller/MajorController.java
@@ -2,13 +2,18 @@ package com.secondproject.secondproject.controller;
 
 
 import com.secondproject.secondproject.dto.MajorInCollegeDto;
+import com.secondproject.secondproject.dto.MajorInsertDto;
+import com.secondproject.secondproject.dto.MajorResponseDto;
+import com.secondproject.secondproject.service.CollegeService;
+import com.secondproject.secondproject.service.LectureService;
 import com.secondproject.secondproject.service.MajorService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -16,12 +21,28 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MajorController {
     private final MajorService majorService;
+    private final CollegeService collegeService;
+    private final LectureService lectureService;
 
     // 단과대학에 속한 학과목록 조회
     @GetMapping("/list") // 매핑주소 수정 필요
     public List<MajorInCollegeDto> majorListByCollege(@RequestParam("college_id") Long collegeId){
         List<MajorInCollegeDto> majorList = majorService.getMajorListByCollege(collegeId);
         return majorList;
+    }
+
+    // 학과 등록
+    @PostMapping("/insert")
+    public ResponseEntity<MajorResponseDto> insert(@RequestBody @Valid MajorInsertDto majorInsertDto){
+        MajorResponseDto responseDto= majorService.insertMajor(majorInsertDto);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .replacePath("/major/{id}")
+                .buildAndExpand(responseDto.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(null);
     }
 }
 
